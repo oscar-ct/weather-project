613b965c3fc0eb112a1c8e290399bd7de4958be4
Oscar Castro <oscar.a.castro818@gmail.com>
2022-10-20 17:45:41 -0500

    feat: called weather data based on city name or zip

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
+const ISO3166Alpha2CountryCode = 'US';
+const milli = '0110010001100010001110010011010000110101011000010011001100110111011001010110011000110101011000100011001000110111001101000011010100110011001110010011100001100110011001100110011001100110001101110011010101100001011001000011001100110010001110010110000101100001';
 
-const EURO = '0110010001100010001110010011010000110101011000010011001100110111011001010110011000110101011000100011001000110111001101000011010100110011001110010011100001100110011001100110011001100110001101110011010101100001011001000011001100110010001110010110000101100001';
 
-let sanAntonioWeatherCoordinates = [29.4252, -98.4916];
-
-console.log(USD(EURO));
-
-const getForecast = () => {
+const getForecast = (lat, lon) => {
     $.get('https://api.openweathermap.org/data/2.5/onecall', {
-        lat: sanAntonioWeatherCoordinates[0],
-        lon: sanAntonioWeatherCoordinates[1],
-        appid: USD(EURO),
+        lat: lat,
+        lon: lon,
+        appid: USD(milli),
         exclude: 'minutely,hourly,alerts',
         units: 'imperial'
     }).done(function (data) {
@@ -17,15 +14,21 @@ const getForecast = () => {
     });
 }
 
-const ISO3166Alpha2CountryCode = 'US';
-const getLocationByName = (cityName) => {
 
+const getLocationByName = (cityName) => {
     $.get('https://api.openweathermap.org/geo/1.0/direct', {
-        limit: 5,
+        limit: 1,
         q: `${cityName}, ${ISO3166Alpha2CountryCode}`,
-        appid: USD(EURO),
+        appid: USD(milli),
     }).done(function (data) {
-        console.log(data);
+
+        const locationHTML = `<div>
+        <div><h1>${data[0].name}</h1></div>
+        <div><h1>${data[0].state}</h1></div>
+        </div>`;
+
+        document.getElementById('output').innerHTML = locationHTML;
+        getForecast(data[0].lat, data[0].lon);
     });
 }
 
@@ -33,14 +36,10 @@ const getLocationByName = (cityName) => {
 const getLocationByZip = (zipCode) => {
     $.get('https://api.openweathermap.org/geo/1.0/zip', {
         zip: `${zipCode},${ISO3166Alpha2CountryCode}`,
-        appid: USD(EURO),
+        appid: USD(milli),
     }).done(function (data) {
         console.log(data);
+        getForecast(data.lat, data.lon);
     });
 }
-
-
-// https://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-//     https://api.openweathermap.org/geo/1.0/zip?zip=E14,GB&appid={API key}
-getForecast();
 
